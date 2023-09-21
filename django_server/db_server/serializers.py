4dd1177351ba760c9aad5bf907ab760d4ed1f8c4
Tran Hung <tranviethung912002@gmail.com>
2023-09-21 09:52:47 +0700

    .

--- django_server/db_server/serializers.py
+++ django_server/db_server/serializers.py
@@ -1,22 +1,30 @@
 from rest_framework import serializers
-from .models import User,Register,Device
+from .models import User, Register, Device
+
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
-        fields = ['user_id', 'username', 'password', 'gender', 'email', 'date_of_birth', 'phone_number', 'balance', 'user_role']
+        fields = [
+            "user_id",
+            "username",
+            "password",
+            "gender",
+            "email",
+            "date_of_birth",
+            "phone_number",
+            "balance",
+            "user_role",
+        ]
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Register
-        fields = ['token', 'user_id']
+        fields = ["token", "user_id"]
+
 
 class DeviceSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Device
-        fields = ['device_id', 'user_id', 'device_name', 'plate_no']
-
+        fields = ["device_id", "user_id", "device_name", "plate_no"]
--- django_server/db_server/urls.py
+++ django_server/db_server/urls.py
@@ -3,9 +3,9 @@ from .views import UserCreate, UserList, UserDetail, UserUpdate, UserDelete
 
 
 urlpatterns = [
-    path('create/', UserCreate.as_view(), name='create-User'),
-    path('', UserList.as_view()),
-    path('<int:pk>/', UserDetail.as_view(), name='retrieve-User'),
-    path('update/<int:pk>/', UserUpdate.as_view(), name='update-User'),
+    path("create/", UserCreate.as_view(), name="create-User"),
+    path("", UserList.as_view()),
+    path("<int:pk>/", UserDetail.as_view(), name="retrieve-User"),
+    path("update/<int:pk>/", UserUpdate.as_view(), name="update-User"),
     # path('delete/<int:pk>/', UserDelete.as_view(), name='delete-User')
 ]
