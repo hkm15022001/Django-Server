ede920b0c06e0239556783f5fb2b1d37c9e04bea
Duong <hoatungduong12@gmail.com>
2023-09-21 16:17:09 +0700

    COOKKK

--- django_server/django_server/settings.py
+++ django_server/django_server/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import dj_database_url
-from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -66,12 +66,12 @@ WSGI_APPLICATION = "django_server.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # DATABASES = {
 #     "default": {
@@ -84,9 +84,9 @@ WSGI_APPLICATION = "django_server.wsgi.application"
 #     }
 # }
 
-DATABASES = {
-    "default": dj_database_url.parse(config("DATABASE_URL"))
-}
+# DATABASES = {
+#     "default": dj_database_url.parse(config("DATABASE_URL"))
+# }
 
 
 # Password validation
